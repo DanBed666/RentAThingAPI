67720b9acb3065bc867be50ae08e89ad821f6631
DanBed666 <daniel@blockether.io>
2026-02-12 00:23:58 +0100

    Add users csv file, and reading data from file

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    excel_data = pandas.read_csv('data/users.csv')
+
+    for e in excel_data.values:
+        print(e)
