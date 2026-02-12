d0051d035bcff5d8fb402f5991ad9725f9e65701
DanBed666 <daniel@blockether.io>
2026-02-12 13:29:05 +0100

    Add GET users endpoint and CSV loading function

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pandas
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+def get_users():
+
+    users_list = []
+    df = pandas.read_csv('data/users.csv')
+
+    for i in range(df.shape[0]):
+        users_list.append(str(df['id'][i]) + " " + df['name'][i] + " " + df['surname'][i] + " " + str(df['age'][i]))
+
+    return users_list
+
 @app.get("/")
 async def root():
-    return {"message": "Hello WOrld"}
+    return {"message": "Homepage"}
 
 
-if __name__ == '__main__':
+@app.get("/users")
+async def root():
+    return JSONResponse(content=get_users(), status_code=200)
 
-    excel_data = pandas.read_csv('data/users.csv')
-
-    for e in excel_data.values:
-        print(e)
