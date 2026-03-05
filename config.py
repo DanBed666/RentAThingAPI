3b93c4ac2e0a15de551ed37235c997006f519bd4
DanBed666 <daniel@blockether.io>
2026-03-05 15:33:19 +0100

    Add getting default variables from env file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv('xvariables.env')
+
+def get_var(key_name):
+
+    try:
+        return os.environ[key_name]
+    except KeyError as e:
+        raise KeyError(f"Invalid key: {e}")
--- models.py
+++ models.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
+from config import get_var
 
 @dataclass
 class User:
 
     uId: int = 0
-    name: str = "Brak"
-    surname: str = "Brak"
-    username: str = "Brak"
-    registerDate: str = "Brak"
-    birthDate: str = "Brak"
-    profilePhotoLink: str = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+    name: str = get_var("DEFAULT_INFO")
+    surname: str = get_var("DEFAULT_INFO")
+    username: str = get_var("DEFAULT_INFO")
+    registerDate: str = get_var("DEFAULT_INFO")
+    birthDate: str = get_var("DEFAULT_INFO")
+    profilePhotoLink: str = get_var("DEFAULT_PROFILE")
 
 
 @dataclass
@@ -17,8 +18,8 @@ class Offer:
 
     userId: int = 0
     offerId: int = 0
-    title: str = "Brak"
-    description: str = "Brak"
+    title: str = get_var("DEFAULT_INFO")
+    description: str = get_var("DEFAULT_INFO")
     price: float = 0.0
-    offerCreateDate: str = "Brak"
+    offerCreateDate: str = get_var("DEFAULT_INFO")
     isOfferEnd: bool = True
