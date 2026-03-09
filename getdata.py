5945aa47f300e6528c8b8e9920975a92287e7ca6
DanBed666 <daniel@blockether.io>
2026-03-09 16:53:22 +0100

    Refactor: rename function arguments

--- getdata.py
+++ getdata.py
@@ -2,39 +2,39 @@ import dataclasses
 import pandas
 
 
-def get_all(filename, classname):
+def get_all(file_name, class_name):
 
     users_list = []
-    df = pandas.read_csv(filename)
+    df = pandas.read_csv(file_name)
 
     for i in range(df.shape[0]):
 
-        element = to_obj(df.iloc[i], classname)
+        element = to_obj(df.iloc[i], class_name)
         users_list.append(element.__dict__)
 
     return users_list
 
 
-def get_by_id(filename, classname, element_id):
+def get_by_id(file_name, class_name, element_id):
 
-    df = pandas.read_csv(filename)
+    df = pandas.read_csv(file_name)
     element = ""
 
     for i in range(df.shape[0]):
 
         if df.iloc[i]['id'] == element_id:
-            element = to_obj(df.iloc[i], classname)
+            element = to_obj(df.iloc[i], class_name)
             break
 
     return element.__dict__
 
-def to_obj(data, classname):
+def to_obj(data, class_name):
 
     element_dict = {}
 
     types = [int, bool, float]
 
-    for field in dataclasses.fields(classname):
+    for field in dataclasses.fields(class_name):
 
         if not pandas.isna(data[field.name]):
             for t in types:
@@ -49,4 +49,4 @@ def to_obj(data, classname):
         else:
             element_dict[field.name] = field.default
 
-    return classname(**element_dict)
+    return class_name(**element_dict)
